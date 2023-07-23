1f1c42f2ab4560487b6c310b0893b5d894575510
RNDpacman <dvmn@riseup.net>
2023-07-23 22:22:06 +0300

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DB_ALFIS_PATH = os.path.join(os.environ.get('ALFIS_DB_PATH', '.'), os.environ.ge
 
 API_TOKEN = os.environ.get('BOT_API_TOKEN', 'token')
 
-CHECK_SECONDS = int(os.environ.get('SCHEDULE_SECONDS', 50))
+CHECK_SECONDS = int(os.environ.get('SCHEDULE_SECONDS', 60))
 
 ALERT_SECONDS = int(os.environ.get('ALERT_SECONDS', 180))
 
@@ -14,7 +14,7 @@ TEXT_MSG = "Last Alfis Block is: "
 
 TEXT_5_MSG = "Block #{} multiple of 5"
 
-TEXT_ALERT_MSG = """Внимание!  @Revertron, @R4SAS и @dradanudanay
+TEXT_ALERT_MSG = """Внимание!  @Revertron, @R4SAS, @dradanudanay и другие многоуважаемые Доны.
 После блока с данными/подписями #{} более {} минут не приходят блоки с подписями!"""
 
 DB_FILE = 'support.db'
